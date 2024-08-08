af9189b9f84ba0f53b70805c54caa117bde51a05
Tiago Garbi <tgbmateriais@gmail.com>
2024-08-08 20:13:06 -0300

    feature: include logic to open/close modal

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,20 @@ const scheduling = [
         date: '',
         hour: '',
     }
-]
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.querySelector('.form-modal');
+    const btnCreate = document.querySelector('.btn-create');
+    const form = document.querySelector('.form-modal form');
+
+    btnCreate.addEventListener('click', () => {
+        modal.style.display = 'flex';
+    });
+
+    modal.addEventListener('click', (event) => {
+        if (!form.contains(event.target)) {
+            modal.style.display = 'none';
+        }
+    });
+});
